6a6f3f63d9f39a3c0f3a7cae69387fd0d6db4ba5
Kavija Sapukotana <it21333620@my.sliit.lk>
2023-10-10 20:54:31 +0530

    User base complete

--- db_fxns.py
+++ db_fxns.py
@@ -1,4 +1,6 @@
 # DB
+import csv
+import os
 import sqlite3
 conn = sqlite3.connect('data.db',check_same_thread=False)
 c = conn.cursor()
@@ -104,6 +106,26 @@ def save_item_rating(username, item_name, rating):
         # Insert the rating into the item_rating table
         cursor.execute("INSERT INTO item_rating (UserID, UserName, ItemName, Rating) VALUES (?, ?, ?, ?)",
                        (get_user_id(username), username, item_name, rating))
+        cursor.execute("SELECT UserID, UserName, ItemName, Rating FROM item_rating ORDER BY RowID DESC LIMIT 1")
+        last_updated_data = cursor.fetchone()
+        if last_updated_data:
+            # Construct the file path
+            file_path = os.path.join('data', 'rating.csv')
+
+            # Write data to the CSV file
+            if os.path.exists(file_path):
+                # Read existing data from the CSV file
+                with open(file_path, 'r', newline='', encoding='utf-8') as file:
+                    existing_data = list(csv.reader(file))
+
+                # Append the new data to the existing data
+                existing_data.append(last_updated_data)
+
+                # Write all data (including existing and new) back to the file
+                with open(file_path, 'w', newline='', encoding='utf-8') as file:
+                    writer = csv.writer(file)
+                    writer.writerows(existing_data)
+                    
         # Commit the changes
         conn.commit()
         return True  # Return True to indicate successful saving of rating
@@ -120,3 +142,103 @@ def get_user_id(username):
         return user_id[0]
     else:
         return None
+    
+
+
+"""
+# Preprocess the data
+                                def preprocess_data(data):
+                                    user_items = defaultdict(list)
+                                    for row in data:
+                                        _,user_name,  _, item_name, _, _, _, _ = row
+                                        user_items[user_name].append(item_name)
+                                    return user_items
+
+                                #Build recommendation model
+                                def build_recommendation_model(user_items):
+                                    all_items = set()
+                                    for items in user_items.values():
+                                        all_items.update(items)
+
+                                    item_to_index = {item: i for i, item in enumerate(all_items)}
+
+                                    user_item_matrix = []
+                                    for user, items in user_items.items():
+                                        user_vector = [0] * len(all_items)
+                                        for item in items:
+                                            user_vector[item_to_index[item]] = 1
+                                        user_item_matrix.append(user_vector)
+
+                                    user_item_matrix = np.array(user_item_matrix)
+
+                                    similarity_matrix = cosine_similarity(user_item_matrix, user_item_matrix)
+
+                                    return similarity_matrix, item_to_index,user_item_matrix
+
+                                #Generate recommendations for a specific user
+                                def get_recommendations(user_id, user_items, similarity_matrix, item_to_index,user_item_matrix):
+                                    user_index = list(user_items.keys()).index(user_id)
+                                    user_similarities = similarity_matrix[user_index]
+                                    similar_users_indices = user_similarities.argsort()[::-1][1:]  
+
+                                    recommendations = set()
+                                    for idx in similar_users_indices:
+                                        for item_idx, val in enumerate(user_item_matrix[idx]):
+                                            if val == 1 and item_idx not in user_item_matrix[user_index]:
+                                                recommendations.add(item_idx)
+
+                                    recommended_items = [item for item, idx in item_to_index.items() if idx in recommendations]
+
+                                    return recommended_items
+
+
+                                
+                                def get_recommendation_details(recommendations):
+                                    details = []
+                                    for item_idx in recommendations:
+                                        # Get item details from the corresponding CSV file
+                                        category = "Men's"  # Assuming it's Men's category
+                                        if category == "Men's":
+                                            df_sugg = pd.read_csv("data/men.csv")
+                                        elif category == "Women's":
+                                            df_sugg = pd.read_csv("data/women.csv")
+                                        elif category == "Children's":
+                                            df_sugg = pd.read_csv("data/kids.csv")
+
+                                        # Extract relevant details (image_url, price)
+                                        item_details = df_sugg.iloc[item_idx]
+                                        image_url = item_details['image_url']
+                                        price = item_details['price']
+
+                                        details.append((image_url, price))
+
+                                    return details
+                                
+                                def display_recommendations_grid(details):
+                                    for image_url, price in details:
+                                        st.image(image_url, caption=f'Price: ${price}', use_column_width=True)
+                                                                                                
+                                data = view_purchase_history(username)
+
+                                user_items = preprocess_data(data)
+                                similarity_matrix, item_to_index,user_item_matrix = build_recommendation_model(user_items)
+                                
+                                
+                                # Get recommendations for a user
+                                recommendations = get_recommendations(username, user_items, similarity_matrix, item_to_index,user_item_matrix)
+                                
+        
+                                # Step 5: Get recommendation item details
+                                details = get_recommendation_details(recommendations)
+
+                                # Step 6: Display recommendations in grid pattern
+                                st.title(f"Recommended Items for {username}")
+                                display_recommendations_grid(details)
+
+                                def display_matrix(matrix):
+                                    for row in matrix:
+                                        print(row)
+
+                                # Assuming user_item_matrix is defined
+                                display_matrix(user_item_matrix) 
+"""
